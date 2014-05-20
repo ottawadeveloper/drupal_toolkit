191aeb381fe7d81dab6a4fddfb9ef4b9d03420f3
ottawadeveloper <rturnbul@uottawa.ca>
2014-05-20 10:32:56 -0400

    work on help commands and the list command

---
+++ lib/plugins/drupal-site-list/constants.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @file Defines the constants for the drupal-site-list plugin.
+ */
+
+/**
+ * String token for displaying the column title "Build Name".
+ * 
+ * @ingroup strings
+ */
+define('STRING_DISPLAY_BUILD_NAME', 'drupal-site-list.display.build-name');
+
+/**
+ * String token for displaying the column title "Drush Alias".
+ * 
+ * @ingroup strings
+ */
+define('STRING_DISPLAY_DRUSH_ALIAS', 'drupal-site-list.display.drush-alias');
+
+/**
+ * String token for displaying the column title "Primary URL".
+ * 
+ * @ingroup strings
+ */
+define('STRING_DISPLAY_PRIMARY_URL', 'drupal-site-list.display.primary-url');
