1c265a0e75ceaac937e188c02f8d7ebe71ee65b0
ottawadeveloper <rturnbul@uottawa.ca>
2014-05-21 12:01:34 -0400

    added subsite-refresh and site-refresh commands

--- lib/plugins/drupal-site-refresh/constants.php
+++ lib/plugins/drupal-site-refresh/constants.php
@@ -2,3 +2,17 @@
 /**
  * @file Defines several useful constants for this plugin.
  */
+
+/**
+ * Event fired to refresh a site's configuration.
+ * 
+ * @ingroup events
+ */
+define('EVENT_SITE_REFRESH_CONFIGURATION', 'refresh-site');
+
+/**
+ * Event fired to refresh a subsite's configuration.
+ * 
+ * @ingroup events
+ */
+define('EVENT_SUBSITE_REFRESH_CONFIGURATION', 'refresh-subsite');
