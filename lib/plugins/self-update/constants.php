29e21925b32fcc58268d582db46d118bf90a2206
ottawadeveloper <rturnbul@uottawa.ca>
2014-05-26 09:50:58 -0400

    added self-update functionality

--- lib/plugins/self-update/constants.php
+++ lib/plugins/self-update/constants.php
@@ -1,4 +1,25 @@
 <?php
 /**
- * @file Provides several useful constants for this plugin.
+ * @file Defines several useful constants for this project.
  */
+
+/**
+ * Fired to update the core of DTK.
+ * 
+ * @ingroup events
+ */
+define('EVENT_SELF_UPDATE_CORE', 'self-update-core');
+
+/**
+ * Fired to allow plugins to update themselves after core has 
+ * been updated.
+ * 
+ * @ingroup events
+ */
+define('EVENT_SELF_UPDATE_EXTRAS', 'self-update-extras');
+
+/**
+ * String token to display when there is an error during the update
+ * process.
+ */
+define('STRING_SELF_UPDATE_ERROR', 'self-update.error.on-update');
