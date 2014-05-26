caf22b74ac9f2ce310574f47008d8babbd8bc764
ottawadeveloper <rturnbul@uottawa.ca>
2014-05-26 10:10:05 -0400

    defined the site-test command

--- lib/plugins/drupal-site-test/constants.php
+++ lib/plugins/drupal-site-test/constants.php
@@ -2,3 +2,10 @@
 /**
  * @file Provides several useful constants for this plugin.
  */
+
+/**
+ * Event fired to generate test reports.
+ * 
+ * @ingroup events
+ */
+define('EVENT_GENERATE_TESTS', 'run-tests');
