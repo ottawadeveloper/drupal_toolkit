f6501ba2ca63d3dc366a3fd580c351d640e38e1a
ottawadeveloper <rturnbul@uottawa.ca>
2014-05-26 10:05:50 -0400

    final tweaks to the self-update functionality

--- lib/plugins/self-update/constants.php
+++ lib/plugins/self-update/constants.php
@@ -21,5 +21,28 @@ define('EVENT_SELF_UPDATE_EXTRAS', 'self-update-extras');
 /**
  * String token to display when there is an error during the update
  * process.
+ * 
+ * @ingroup strings
  */
 define('STRING_SELF_UPDATE_ERROR', 'self-update.error.on-update');
+
+/**
+ * String token to display when there is an error pulling DTK from git.
+ * 
+ * @ingroup strings
+ */
+define('STRING_FAILURE_PULLING_DTK', 'self-update.failure.on-update');
+
+/**
+ * String token to display when DTK was pulled without errors from git.
+ * 
+ * @ingroup strings
+ */
+define('STRING_SUCCESS_PULLING_DTK', 'self-update.success.on-update');
+
+/**
+ * String token to display just before DTK is pulled from git.
+ * 
+ * @ingroup strings
+ */
+define('STRING_WORKING_PULLING_DTK', 'self-update.working.on-update');
