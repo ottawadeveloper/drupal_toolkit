9ceaecf7646bea4934f72445571480367efcdbda
ottawadeveloper <rturnbul@uottawa.ca>
2014-05-28 11:54:58 -0400

    documentation of phploc constants

--- lib/plugins/phploc/constants.php
+++ lib/plugins/phploc/constants.php
@@ -13,3 +13,24 @@
  * @ingroup config
  */
 define('CONFIG_PHPLOC_FILES', 'phploc-file-patterns');
+
+/**
+ * String token for when phploc generation is about to happen.
+ * 
+ * @ingroup strings
+ */
+define('STRING_WORKING_PHPLOC', 'phploc.working.phploc');
+
+/**
+ * String token for when phploc generation went fine.
+ * 
+ * @ingroup strings
+ */
+define('STRING_SUCCESS_PHPLOC', 'phploc.success.phploc');
+
+/**
+ * String token for when phploc failed.
+ * 
+ * @ingroup strings
+ */
+define('STRING_FAILURE_PHPLOC', 'phploc.failure.phploc');
