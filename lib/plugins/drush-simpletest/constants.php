334e340ae779a4ce1a552120b18ff76f2da8884f
ottawadeveloper <rturnbul@uottawa.ca>
2014-05-28 11:03:25 -0400

    initial working build of simpletest

---
+++ lib/plugins/drush-simpletest/constants.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @file Defines useful constants for this plugin.
+ */
+
+define('OPTION_SIMPLETEST_CASES', 'simple-test-classes');
+
+define('OPTION_SIMPLETEST_METHODS', 'simple-test-methods');
+
+define('OPTION_SIMPLETEST_DIRTY', 'simple-test-dirty');
+
+define('CONFIG_SIMPLETEST_DIRTY', 'simple-test-dirty');
+
+define('STRING_FAILURE_SIMPLE_TEST', 'drush-simpletest.failure.testing');
+
+define('STRING_SUCCESS_SIMPLE_TEST', 'drush-simpletest.success.testing');
+
+define('STRING_WORKING_SIMPLE_TEST', 'drush-simpletest.working.testing');
