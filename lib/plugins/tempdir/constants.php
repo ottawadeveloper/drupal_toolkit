621c2c04c453d9c98cfb73f021f31312fffa3718
ottawadeveloper <rturnbul@uottawa.ca>
2014-07-22 10:14:31 -0400

    finished temporary directory cleanup plugin

--- lib/plugins/tempdir/constants.php
+++ lib/plugins/tempdir/constants.php
@@ -2,3 +2,41 @@
 /**
  * @file Useful constants for this plugin.
  */
+
+/**
+ * Config value for the location of the temp directory that will be wiped
+ * when starting a build or update process.
+ * 
+ * @ingroup config
+ */
+define('CONFIG_TEMPDIR_DIRECTORY', 'temp-directory');
+
+/**
+ * Config flag for whether or not to wipe the temp directory specified in
+ * CONFIG_TEMPDIR_DIRECTORY. Defaults to FALSE.
+ * 
+ * @ingroup config
+ */
+define('CONFIG_TEMPDIR_CLEAR', 'clear-temp-directory');
+
+/**
+ * String token for when the temp directory was wiped.
+ * 
+ * @ingroup strings
+ */
+define('STRING_SUCCESS_TEMPDIR_CLEANUP', 'tempdir.success.cleanup');
+
+/**
+ * String token for when there was an error while wiping the temp directory.
+ * 
+ * @ingroup strings
+ */
+define('STRING_ERROR_TEMPDIR_CLEANUP', 'tempdir.error.cleanup');
+
+/**
+ * String token for the message to display before attempting to wipe the 
+ * temporary directory.
+ * 
+ * @ingroup strings
+ */
+define('STRING_WORKING_TEMPDIR_CLEANUP', 'tempdir.working.cleanup');
