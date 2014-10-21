7f51fb8e1c245059bf0f38ca120ed80157a0fe80
Robert Turnbull <rturnbul@uottawa.ca>
2014-10-21 14:51:43 -0400

    documented the site-info commands

--- lib/plugins/drupal-site-info/constants.php
+++ lib/plugins/drupal-site-info/constants.php
@@ -16,3 +16,17 @@ define('EVENT_GET_INFO', 'info-get');
  * @ingroup strings
  */
 define('STRING_INFO_HEADER', 'drupal_site_info.info.header');
+
+/**
+ * String token for describing the site-info command.
+ * 
+ * @ingroup strings
+ */
+define('STRING_HELP_SITE_INFO_DESCRIPTION', 'help.command-desc.site-info');
+
+/**
+ * String token for describing the subsite-info command.
+ * 
+ * @ingroup strings
+ */
+define('STRING_HELP_SUBSITE_INFO_DESCRIPTION', 'help.command-desc.subsite-info');
