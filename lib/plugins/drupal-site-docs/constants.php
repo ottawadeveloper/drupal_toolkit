caa86d689e10ed62c8c23c563fd76687dd760e34
Robert Turnbull <rturnbul@uottawa.ca>
2014-10-21 14:44:47 -0400

    described the site-doc command

--- lib/plugins/drupal-site-docs/constants.php
+++ lib/plugins/drupal-site-docs/constants.php
@@ -13,5 +13,14 @@ define('EVENT_GENERATE_DOCUMENTATION', 'generate-docs');
 /**
  * Event fired before documentation is generated to allow installs to
  * do something, such as generate a documentation file.
+ * 
+ * @ingroup events
  */
 define('EVENT_PRE_DOCUMENTATION', 'pre-generate-docs');
+
+/**
+ * String token for describing the site-doc command.
+ * 
+ * @ingroup strings
+ */
+define('STRING_HELP_SITE_DOC_DESCRIPTION', 'help.command-desc.site-doc');
