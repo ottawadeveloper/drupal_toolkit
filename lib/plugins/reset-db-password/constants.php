062209ad0ea6dd2bd91e02676a6e6886c779ba9c
Robert Turnbull <rturnbul@uottawa.ca>
2014-11-05 09:56:37 -0500

    utility command for password resets

---
+++ lib/plugins/reset-db-password/constants.php
@@ -0,0 +1,41 @@
+<?php
+
+define('OPTION_RESET_MANUAL_PASSWORD', 'manual-password');
+
+define('EVENT_RESET_DRUPAL_PASSWORD', 'reset-passwords.make-it-so');
+
+define('EVENT_RESET_PASSWORD_ALL_START', 'reset-passwords-all.start');
+
+define('EVENT_RESET_PASSWORD_ALL_FINISH', 'reset-passwords-all.finish');
+
+define('EVENT_REFRESH_DRUPAL_CONFIGURATION', 'refresh_config.make-it-so');
+
+define('EVENT_PRE_RESET_DRUPAL_PASSWORD', 'reset_passwords.pre-reset');
+
+define('EVENT_POST_RESET_DRUPAL_PASSWORD', 'reset_passwords.post-reset');
+
+define('STRING_HELP_SITE_RESET_PASSWORD_DESCRIPTION', 'reset_pass.command_desc.site-reset');
+
+define('STRING_HELP_SUBSITE_RESET_PASSWORD_DESCRIPTION', 'reset_pass.command_desc.subsite-reset');
+
+define('STRING_HELP_RESET_PASSWORDS_ALL_DESCRIPTION', 'reset_pass.command_desc.global-reset');
+
+define('STRING_HELP_OPTION_RESET_MANUAL_PASSWORD', 'reset_pass.option_desc.manual-password');
+
+define('STRING_WORKING_RESET_PASSWORD_ALL_SITE', 'reset_pass.working.site');
+
+define('STRING_WORKING_RESET_PASSWORD_ALL_SUBSITE', 'reset_pass.working.subsite');
+
+define('STRING_ERROR_RESET_PASSWORD_ALL_SITE', 'reset_pass.error.site');
+
+define('STRING_ERROR_RESET_PASSWORD_ALL_SITES', 'reset_pass.error.global');
+
+define('STRING_SUCCESS_RESET_PASSWORD_ALL_SITES', 'reset_pass.success.global');
+
+define('STRING_FAILURE_RESET_PASSWORD', 'reset_pass.failure.reset');
+
+define('STRING_WORKING_RESET_PASSWORD', 'reset_pass.working.reset');
+
+define('STRING_SUCCESS_RESET_PASSWORD', 'reset_pass.success.reset');
+
+define('STRING_ERROR_RESET_EMPTY_PASSWORD', 'reset_pass.error.reset-no-pass');
