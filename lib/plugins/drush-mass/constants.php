4fbe7ffe4bf57c338d51956e39b7c5014115a7c5
Robert Turnbull <rturnbul@uottawa.ca>
2014-11-05 10:51:39 -0500

    added the ability to execute two Drush commands en mass (cc all and vset maintenance mode)

---
+++ lib/plugins/drush-mass/constants.php
@@ -0,0 +1,13 @@
+<?php
+
+define('STRING_HELP_DRUSH_DOWN_COMMAND_DESC', 'drush_mass.help.drush-down');
+
+define('STRING_HELP_DRUSH_UP_COMMAND_DESC', 'drush_mass.help.drush-up');
+
+define('STRING_HELP_CC_ALL_COMMAND_DESC', 'drush_mass.help.cc-all');
+
+define('EVENT_DRUSH_MAINTENANCE_MODE_OFF', 'drush_mass.disengage-maintenance');
+
+define('EVENT_DRUSH_MAINTENANCE_MODE_ON', 'drush_mass.engage-maintenance');
+
+define('EVENT_DRUSH_CACHE_WIPE', 'drush_mass.cache-wipe');
